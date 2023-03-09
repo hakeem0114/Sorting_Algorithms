c0e9ae3a508c1b13f1544e31159e5c697253edb9
hakeem <hakeem0114@gmail.com>
2023-03-09 00:20:14 -0500

    Solution keeps causing browser to crash. Fix after work

--- SortAlgos/bubbleSort.js
+++ SortAlgos/bubbleSort.js
@@ -23,36 +23,6 @@ do{
 */
 
 
-function bubbleSort(array){
-
-    let swapped;
-    do{
-            for(let i=0; i< array.length-1; i++){
-                if(array[i]>array[i+1]){
-                    //swap positions using array destructing
-                     [array[i], array[i+1]] = [array[i+1], array[i]]
-                    swapped = true;
-                }
-                swapped = false;
-            }
-
-    }while(swapped = true)
-    return array
-}
-
-const arr = [-6, 20,8,-2,4]
-console.log(bubbleSort(arr))
-
-
-
-
-
-
-
-
-
-
-
 
 
 
