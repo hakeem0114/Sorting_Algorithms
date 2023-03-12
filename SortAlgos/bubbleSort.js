c87e4395f0ba9494c659c5108e30dee35254c59e
hakeem <hakeem0114@gmail.com>
2023-03-11 20:31:38 -0500

    Finished bubble sort

--- SortAlgos/bubbleSort.js
+++ SortAlgos/bubbleSort.js
@@ -22,8 +22,34 @@ do{
 
 */
 
+function bubbleSort(arr){
+  let swapped  //Function closed variable to keep track of swap state
 
+  do{
+      swapped = false //swapped state is false if for loop fails
 
+      for (let i=0; i< arr.length -1; i++){
+        if(arr[i] > arr[i+1]){
+          //swap 
+          
+          //Temp var to swap 
+          // temp  = arr[i] 
+          // arr[i]  = arr[i+1] 
+          // arr[i+1] = temp 
+
+          //Array destructuring
+          [arr[i], arr[i+1]] = [arr[i+1],arr[i]] //
+          swapped = true //swap state is true if for loop passes
+        }
+      }
+
+  }while (swapped) //Runs as long as swapped is truthy = true
+}
+
+const arr = [8, 20, -2 , 4, -6, 5]
+
+bubbleSort(arr)
+console.log(arr)
 
 
 console.log("***************************")
